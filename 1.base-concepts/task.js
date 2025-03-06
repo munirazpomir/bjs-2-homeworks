557f5346b310dea65a1ad79ab52408920340c67c
Munir <bred04li@gmail.com>
2025-03-06 14:36:49 +0300

    Выполнение 1 задания

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = b ** 2 - 4 * a * c;
+
+	if (d < 0) {
+		arr = [];
+	} else if (d === 0) {
+		arr = [-b / (2 * a)];
+	} else if (d > 0) {
+		arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+	}
+	return arr;
 }
 
+console.log(solveEquation());
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let p = (percent / 100) / 12;
+	let s = amount - contribution;
+	let monthlyPay = s * (p + (p / (((1 + p) ** countMonths) - 1)));
+
+
+	return (monthlyPay * countMonths).toFixed(2);
 }
+
+console.log(calculateTotalMortgage());
